08bf404fa929c40a7a6649f1f27c1e85baa4e5c7
Nicanor Perera <nicanorperera@gmail.com>
2012-02-21 09:48:07 -0300

    Version 0.3.2 - Agregada Seguridad

--- proyecto/config/initializers/dragonfly.rb
+++ proyecto/config/initializers/dragonfly.rb
@@ -7,8 +7,8 @@ app.datastore = Dragonfly::DataStorage::S3DataStore.new
 app.datastore.configure do |c|
   c.bucket_name = "URL" if Rails.env.production?
   c.bucket_name = 'development' if Rails.env.development?
-  c.access_key_id = 'AKIAJLFRFWLLZBKPNOZA'
-  c.secret_access_key = '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'
+  c.access_key_id = APP_CONFIG[:access_key_id]
+  c.secret_access_key = APP_CONFIG[:secret_access_key]
 end
 
 app.configure_with(:imagemagick)
